5c649802edbee0c2d9ecaab04d079bf7dca85710
Aritra Banerjee <banerjeearitra63@gmail.com>
2025-05-06 05:33:20 +0530

    update-1.0

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $(".menu-icon").click(function(){
+        $(".nav-items").toggleClass("on");
+        $(".menu-icon i").toggleClass("fa-bars fa-xmark");
+    });
+});
